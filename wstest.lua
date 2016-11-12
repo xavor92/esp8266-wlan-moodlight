587bc63ae04c64992ff43bfb07500076b90c73dd
xavor92 <no@one.com>
2016-11-12 14:30:02 +0100

    Cleaned file, colors into single variables

--- wstest.lua
+++ wstest.lua
@@ -1,9 +1,18 @@
 print("start wstest")
 ws2812.init()
-local i, buffer = 0, ws2812.newBuffer(64, 3); buffer:fill(0, 0, 0); tmr.alarm(0, 50, 1, function()
-        i=i+1
-        buffer:fade(2)
-        buffer:set(i%buffer:size()+1, 255, 255, 255)
-        ws2812.write(buffer)
-end)
+local i = 0
+local buffer = ws2812.newBuffer(64, 3)
+color_g, color_r, color_b = 0,0, 255
+buffer:fill(0, 0, 0)
+print(colors)
+
+function LED_tmr_fct()
+    buffer:fade(2)
+    buffer:set(i%buffer:size()+1, color_g, color_r, color_b)
+    ws2812.write(buffer)
+    i=i+1
+    end
+    
+tmr.alarm(0, 50, tmr.ALARM_AUTO, LED_tmr_fct)
+        
 print("end wstest")
