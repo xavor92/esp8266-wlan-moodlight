25758be5bac6b68a29159188836dc6b3fb0ca1a2
xavor92 <no@one.com>
2016-11-12 14:30:18 +0100

    modify color from web

--- webserver.lua
+++ webserver.lua
@@ -1,8 +1,6 @@
 print("start webserver")
 wifi.setmode(wifi.STATION)
---wifi.sta.config("Speedlink-256","einsehrsicherespasswort")
-wifi.sta.config("Unity WLAN","hanspeter123")
-print(wifi.sta.getip())
+print(wifi.sta.getip()) 
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
@@ -19,20 +17,15 @@ srv:listen(80,function(conn)
             end
         end
         buf = buf.."<h1> ESP8266 Web Server</h1>";
-        buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
-        buf = buf.."<p>GPIO2 <a href=\"?pin=ON2\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF2\"><button>OFF</button></a></p>";
-        buf = buf..'<input type="range"  min="0" max="100" />'
+        buf = buf.."<p>LEDs <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>"
         local _on,_off = "",""
         if(_GET.pin == "ON1")then
-              print("call wstest")
-              dofile('wstest.lua')
+            print("turn leds off")
+            color_r = 255
+            color_b = 0    
         elseif(_GET.pin == "OFF1")then
-              --gpio.write(led1, gpio.LOW);
-              print('lol');
-        elseif(_GET.pin == "ON2")then
-              gpio.write(led2, gpio.HIGH);
-        elseif(_GET.pin == "OFF2")then
-              gpio.write(led2, gpio.LOW);
+              color_r = 0
+              color_b = 255
         end
         client:send(buf);
         client:close();
