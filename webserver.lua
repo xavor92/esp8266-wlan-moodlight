2ff375575d71002ecfed31abd5a66879c6b68833
xavor92 <owestermann@googlemail.com>
2016-12-15 21:14:21 +0100

    added slider for red part

--- webserver.lua
+++ webserver.lua
@@ -1,3 +1,19 @@
+script = [[
+<script>
+function httpGetAsync()
+{
+    var x = document.getElementById("greyscale").value;
+    var xmlHttp = new XMLHttpRequest();
+    var theUrl = "?pin=" + x
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            ;
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}</script>
+]]
+
 print("start webserver")
 wifi.setmode(wifi.STATION)
 print(wifi.sta.getip()) 
@@ -18,18 +34,24 @@ srv:listen(80,function(conn)
         buf = buf.."<h1> ESP8266 Web Server</h1>";
         buf = buf.."<p>LEDs <a href=\"?pin=RED\"><button>RED</button></a>&nbsp;"
         buf = buf.."<a href=\"?pin=BLUE\"><button>BLUE</button></a>&nbsp;"
-        buf = buf.."<a href=\"?pin=RND\"><button>RANDOM</button></a></p>"
-        local _on,_off = "",""
+        buf = buf.."<a href=\"?pin=OFF\"><button>OFF</button></a></p>"
+        buf = buf.."<input id=\"greyscale\" type=\"range\" min=\"0\" max=\"255\" oninput=\"httpGetAsync()\" value=\"30\"/><br>"
+        buf = buf..script
+        print("_GET.pin is ", _GET.pin)
         if(_GET.pin == "RED")then
-            LED_runningLight()
             color_r = 255
             color_b = 0
-        elseif(_GET.pin == "BLUE")then
             LED_runningLight()
+        elseif(_GET.pin == "BLUE")then
             color_r = 0
             color_b = 255
-        elseif(_GET.pin == "RND")then
-            LED_rndLight()
+            LED_runningLight()
+        elseif(_GET.pin == "OFF")then
+            LED_Clear()
+        elseif(_GET.pin ~= nil) then
+            color_r = tonumber(_GET.pin)
+            color_b = 0
+            LED_runningLight()
         end
         client:send(buf);
         client:close();
